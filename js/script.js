ff99b7909d2bff367455bdcaf5c52dfb883aeea7
Alassane Sow <alassane@yeeweende.com>
2025-12-03 23:15:24 +0100

    Portfolio opgeschoond en structuur klaargezet

--- js/script.js
+++ js/script.js
@@ -1,13 +1,4 @@
-// https://stackoverflow.com/questions/39993676/code-inside-domcontentloaded-event-not-working
 
-document.addEventListener("DOMContentLoaded", function() {
-  const hash = window.location.hash; // bv. #python
-  if (hash) {
-    document.querySelectorAll("section[id]").forEach(sec => sec.style.display = "none");
-    const active = document.querySelector(hash);
-    if (active) active.style.display = "block";
-  }
-});
 
 
 
